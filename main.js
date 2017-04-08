35296e84580a94ea2dc6bb37986d14f67b4346f7
Yoann Picquenot <yoann.picquenot@epitech.eu>
2017-04-08 19:56:52 +0000

    Added materializecss

--- main.js
+++ main.js
@@ -10,38 +10,54 @@ function redirect() {
     }
 }
 
-function applyHtml() {
+function applyHtml(cb) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementsByTagName('main')[0].innerHTML = this.responseText;
+            document.dispatchEvent(new Event('htmlReady'));
         }
     };
     xhttp.open("GET", window.history.state.urlPath, true);
     xhttp.send();
 }
 
-var connection = function connection() {
-    socket = io.connect('http://55764eff.ngrok.io');
-    window.history.pushState({
-        'urlPath': 'views/chatroom.html'
-    }, null, '/chatroom');
-    applyHtml();
-
-    username = document.forms[0]['username'].value;
-    socket.emit('connected', { username });
-
-    socket.on('message', function(data) {
-        document.getElementsByClassName('msg-container')[0].innerHTML += `<p>${data}</p>`;
-    });
-}
-
 var sendMessage = function sendMessage() {
-    console.log(document.forms);
-    socket.emit('message', {username, message: document.getElementsByName('msg-input')[0].value});
+    socket.emit('message', {
+        username,
+        message: document.getElementsByName('msg-input')[0].value
+    });
+
+    document.getElementsByName('msg-input')[0].value = '';
 }
 
-/*(function() {*/
 redirect();
-applyHtml(window.location.pathname);
-/*})();*/
+applyHtml();
+
+document.addEventListener('htmlReady', function() {
+    if (window.location.pathname === '/login') {
+        document.forms[0].onsubmit = function(e) {
+            username = document.forms[0]['username'].value;
+            e.preventDefault();
+            socket = io.connect('http://localhost:5000');
+            window.history.pushState({
+                'urlPath': 'views/chatroom.html'
+            }, null, '/chatroom');
+            applyHtml();
+            socket.emit('connected', {
+                username
+            });
+            socket.on('message', function(data) {
+                document.getElementsByClassName('msg-container')[0].innerHTML += `
+                <div class='card blue white-text'>
+                <div class='card-content'>${data}</div>
+                </div>
+                `;
+            });
+
+            return false;
+        }
+    } else {
+
+    }
+});
