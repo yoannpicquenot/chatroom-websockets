a987f02bbd9bd9f4d907a31647dca2c0a5614d91
Thibault Desplat <neverlife1@hotmail.fr>
2017-04-08 15:56:46 -0400

    préparation des messages du chat

--- app.js
+++ app.js
@@ -14,12 +14,23 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+  //var toEmit = '<i>Nouvel utilisateur connecté</i>';
+  //io.emit('message', toEmit);
   console.log('Nouvel utilisateur connecté');
+
   socket.on('disconnect', function (data) {
+    //var toEmit = '<i>Un utilisateur s\'est déconnecté</i>';
+    //io.emit('message', toEmit);
     console.log('Un utilisateur s\'est déconnecté');
   });
 
   socket.on('message', function(message) {
-    //io.emit('message', message);
+    //var toEmit = '<b>' + message.username + ' :</b> ' + message.message;
+    //io.emit('message', toEmit);
+  });
+
+  socket.on('change username', function(username) {
+    //var toEmit = '<i>"' + username.old + '" a changé son nom en "' + username.new + '"</i>';
+    //io.emit('message', toEmit);
   });
 });
